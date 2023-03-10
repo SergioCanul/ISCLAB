d81c81c73c66e6e555dd2be175d4fbff3142331a
SergioCanul <sergio.18070014@itsmotul.edu.mx>
2023-03-09 20:02:14 -0600

    avance

--- src/components/TheWelcome.vue
+++ src/components/TheWelcome.vue
@@ -1,26 +1,37 @@
 <template>
-<div class=" w-full p-4 text-center bg-white border border-gray-200 sm:p-8 dark:bg-slate-100 dark:border-gray-700">
-    <h1 class="mb-2 text-6xl font-bold text-gray-900 dark:text-slate-800 text-left">¿QUE HACEMOS EN ISCLAB?</h1>
+<div class=" w-full p-4 text-center bg-white border border-gray-200 sm:p-8 dark:bg-slate-100 dark:border-slate-100">
+    <h1 class="mb-2 text-6xl font-bold text-gray-900 dark:text-slate-800 text-center">¿QUE HACEMOS EN ISCLAB?</h1>
     <div class="flex items-center container mx-auto items-center">
-        <p class="mb-1 text-base text-gray-500 sm:text-lg dark:text-gray-600 text-left">Desarrollamos aplicaciones con fines de práctica, investigación y responsabilidad social.
+        <p class="mb-1 text-base text-gray-500 sm:text-lg dark:text-gray-600 text-right">Desarrollamos aplicaciones con el fin de obtener práctica, investigación y responsabilidad social.
             <span class="block"></span>
-            Buscamos el desarrollo de nuestros estudiantes a través de la creación de proyectos que sean útiles a la sociedad.
+            Buscamos el desarrollo de nuestros alumnos a través de la creación de proyectos de utilidad a la sociedad.
             <span class="block"></span>
             ¿Tienes un proyecto en mente? Compártelo con nosotros y buscaremos la manera de ayudarte.</p>
-            <img class="h-auto max-w-xs ml-auto" src="@/assets/labprog.jpg" alt="image description">
-</div>
-   
-
-    <div class="items-left justify-left space-y-4 sm:flex sm:space-y-0 sm:space-x-4">
-
-    </div>  
-</div>
-<div class="w-full p-4 text-center bg-white border border-gray-400 rounded-lg shadow sm:p-8 dark:bg-gray-800 dark:border-gray-700">
-    <h5 class="mb-2 text-3xl font-bold text-gray-900 dark:text-white">Work fast from anywhere</h5>
-    <p class="mb-5 text-base text-gray-500 sm:text-lg dark:text-gray-400">Stay up to date and move work forward with Flowbite on iOS & Android. Download the app today.</p>
-    <div class="items-center justify-center space-y-4 sm:flex sm:space-y-0 sm:space-x-4">
+            <img class="h-auto max-w-xs" src="@/assets/labprog.png" alt="image description">
     </div>
+    <div class="items-left justify-left space-y-4 sm:flex sm:space-y-0 sm:space-x-4">
+    </div>
+
+ 
 </div>
 
+<div class=" w-full p-4 text-center bg-white border border-gray-200 sm:p-8 dark:bg-slate-100 dark:border-slate-100">
+    <h1 class="mb-2 text-4xl font-bold text-gray-900 dark:text-slate-800 text-center">¿Pueden crear un sitio web gratuito para mi proyecto?</h1>
+    <div class="flex items-center container mx-auto items-center sm:p-8">
+        <img class="h-auto max-w-xs" src="@/assets/sitioweb.png" alt="image description">
+        <p class="mb-1 text-base text-gray-500 sm:text-lg dark:text-gray-600 text-left">La respuesta corta es sí.
+            <span class="block"></span>
+            Claro que necesitamos saber en qué consiste el sitio que necesitas y tu historia
+            <span class="block"></span>
+            para valorar el apoyo gratuito en la creación y publicación de tu sitio.
+            <span class="block"></span>
+            Averigua las condiciones de un sitio web gratuito, para nosotros será un placer apoyarte.</p>
+            
+    </div>
+    <div class="items-left justify-left space-y-4 sm:flex sm:space-y-0 sm:space-x-4">
+    </div>
+
+ 
+</div>
 
 </template>
